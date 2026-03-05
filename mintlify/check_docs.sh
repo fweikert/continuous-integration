4526939d05d1818df3737715286b8303967b4e67
Florian Weikert <fwe@google.com>
2026-03-05 17:40:07 +0100

    test

--- mintlify/check_docs.sh
+++ mintlify/check_docs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -euxo pipefail
 
+bazel build //src/main/java/com/google/devtools/build/lib:gen_mdx_reference_docs
+
+unzip bazel-bin/src/main/java/com/google/devtools/build/lib/mdx-reference-docs.zip -d "$DOCS_DIR"
+
 cd "$DOCS_DIR"
 
 # Fetch the docs.json file at HEAD, otherwise mintlify fails.
